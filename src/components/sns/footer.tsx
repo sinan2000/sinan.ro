ba6bbf13f0f58fd920ccba9d06bdb6696e17f3b1
Sinan <sinan_ceviker@yahoo.com>
2025-02-28 22:44:37 +0100

    good for now

--- src/components/sns/footer.tsx
+++ src/components/sns/footer.tsx
@@ -37,7 +37,7 @@ export default function SNSFooter() {
                 </div>
 
                 {/* Animated Line */}
-                <div className="absolute bottom-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-emerald-500/20 to-transparent">
+                <div className="absolute bottom-0 left-0 w-full h-[5px] bg-gradient-to-r from-transparent via-emerald-500/20 to-transparent">
                     <div
                         className="absolute inset-0 bg-gradient-to-r from-transparent via-emerald-400/40 to-transparent"
                         style={{
